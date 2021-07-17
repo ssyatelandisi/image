b3441556916e2458c58ccf59b71f1f647eda5392
ssyatelandisi <ssyatelandisi@163.com>
2021-07-17 08:51:33 +0800

    image

--- index.js
+++ index.js
@@ -26657,23 +26657,23 @@ function config (name) {
 },{}],187:[function(require,module,exports){
 (function (Buffer){(function (){
 console.time('耗时')
-const start = new Date()
 const crypto = require('crypto')
 const key = new Buffer.from('0b1e1d000f0b07091d1f04071f1e0407')
 const iv = new Buffer.from('0123456789ABCDEF');
 const algorithm = 'aes-256-cbc';
 const decipher = crypto.createDecipheriv(algorithm, key, iv);
-
+const start_fetch = new Date()
 fetch('https://cdn.jsdelivr.net/gh/ssyatelandisi/image@master/output.jpg').then((res) => res.arrayBuffer()).then(res => {
+    const end_fetch = new Date()
+    const start_encrypto = new Date()
     let decrypted = decipher.update(new Uint8Array(res), 'binary', 'binary');
     decrypted += decipher.final();
     const blob = new Blob([new Buffer.from(decrypted, 'binary')], { type: 'image/jpeg' })
+    const end_encrypto = new Date()
     const url = URL.createObjectURL(blob)
     document.querySelector("#aimg").src = url
+    document.querySelector('#info').innerHTML = `请求耗时：${end_fetch - start_fetch}mm<br/>解密耗时：${end_encrypto - start_encrypto}mm`
 }).finally(() => {
-    const end = new Date()
-    const cost = end - start
-    document.querySelector('#info').innerText = `耗时：${cost}mm`
     console.timeEnd('耗时')
 })
 }).call(this)}).call(this,require("buffer").Buffer)
